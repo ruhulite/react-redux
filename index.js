727b8472db87883130632bbdc35515e9c490d595
Md. Ruhul Amin Khan <39692985+ruhulite@users.noreply.github.com>
2020-03-16 23:11:01 +0600

    multi reducer and middleware

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 //add Redux library
 const redux = require('redux');
+//Middleware: redux logger
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+
+const logger  = reduxLogger.createLogger();
 
 //create action
 const BUY_CAKE = 'BUY_CAKE';
@@ -85,20 +92,24 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
 }
 
 //create store: 1. Holds application state
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+});
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 //2. Allow access to state via getStore()
-console.log('init sate', store.getState());
+console.log('initial sate', store.getState());
 
 //4. Registers listeners via subscribe(listerner)
-store.subscribe(() => console.log('Updated state ', store.getState()));
+const unsubscribe = store.subscribe(() => {});
 
 //3. Allows state to be updated via dispatch(action)
 store.dispatch(buyCake());
 store.dispatch(buyIceCream());
 
 //5. Handlers unregistering of listeners via the function returned by subscribe(listener)
-//unsubscribe();
+unsubscribe();
 
 
 
@@ -117,5 +128,36 @@ Responsibility -
  * 
  * 
  * 
+ Middleware
+ 1. Is the suggested way to extend Redux with custom functionality
+ 2. Provides a third-party extenson point between dispatching an action and the moment it reaches the reducer
+ 3. Use reducer for login, crash reporting, performing asynchronous tasks etc
+ * 
+ *
+ * Actions
+ * Asynchronous Actions
+ * 1. As soon as an action was dispatched, the state was immediately updated
+ * 2. If you dispatch the BUY_CAKE action, the numOfCakes was right away decremented by 1 
+ * Same as BUY_ICECREAM as well
+ * *
+ * *
+ * *
+ * *
+ * 
+ * *
+ * **
+ * 
+ * **
+ * *
+ * 
+ * *
+ * *
+ * *
+ * *
+ * *
+ * *
+ * *
+ * *
+ * *
  * 
  */
